75770bbbbd500337552087a028dc93aad38d1fab
VAlduinV <prime72w@gmail.com>
2023-04-24 19:45:08 +0300

    Add commit

---
+++ Program/__init__.py
@@ -0,0 +1,4 @@
+from .bot_interface import BotInterface
+from .file_sorter import FileSorter
+from .notes_main import main_note_book
+from Personal_assistant.Program.address_book import Contact, AddressBook
